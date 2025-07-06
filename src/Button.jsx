e6da902273d729bd9d774ba715ace1d1380c1a68
stanly-sudo <alys.git24@gmail.com>
2025-07-06 08:16:45 +0800

    inline style

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,10 +1,28 @@
-import styles from './Button/Button.module.css'
+// import styles from './Button/Button.module.css'
 
 function Button() {
 
+  const styles={
+  //  backgroundColor: "red",
+      
+        width: "15rem",
+        height: "5rem",
+        borderRadius: "16px",
+        border: "1px solid black",
+
+        boxShadow: "-2px 2px 0px 1px",
+        
+        backgroundColor: "rgb(19, 235, 19)",
+        textAlign: "center",
+        fontSize: "1rem",
+        cursor: "pointer",
+  }
+
   return (
     <>
-        <button className={styles.Button}>Click me</button>
+        {/* <button className="Button">Click me</button> */} {/* External CSS */}
+        {/* <button className={styles.Button}>Click me</button> */} {/* Module CSS */}
+        <button style={styles}>Click me</button> {/**/}
     </>
   )
 }
